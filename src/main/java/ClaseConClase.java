92675451887aaee4a474c2bedadb7d101f5c9f0c
Ruben Gomez <rubengomez78@gmail.com>
2018-04-18 10:57:17 +0200

    erores

---
+++ src/main/java/ClaseConClase.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ClaseConClase {
+	public static void metodoConClase() {
+		String nombre = "";
+		List lista = new LinkedList();
+		for (int i = 0,j=0;i<10;j++) {
+			System.err.println("Toma");
+		}
+		while(true) {
+			System.out.println("toma2");
+		}
+		
+	}
+
+}
